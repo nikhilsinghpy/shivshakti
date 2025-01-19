470e3481b8f2183e08cdd04d9c076998aa37e148
nikhilsinghpy <nikhilsingh1139@gmail.com>
2025-01-19 11:41:37 +0530

    some

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -9,6 +9,7 @@ import { Speaker } from './speaker/Speaker'
 import { Announcers } from './Announcers/Announcers'
 import { Schedule } from './Schedule/Schedule'
 import { Zones } from './Zones/Zones'
+import img from '../../assets/roundedchart.svg'
 
 // Register required Chart.js components
 ChartJS.register(ArcElement, Tooltip, Legend)
@@ -52,124 +53,79 @@ export const Dashboard = () => {
   const percentageAvailable = 100 - percentageUsed
   return (
     <>
-      <div className="flex  lg:flex-row w-full min-h-screen gap-2">
-        {/* Left Section - 75% width on laptop, full width on smaller screens */}
-        <div className="w-full lg:w-[70%]">
-          <TabComponent navItems={navItems} tabContents={tabContents} />
+      <div class="flex w-full h-screen p-4">
+        <div class="flex-[7]">
+          {/* <TabComponent navItems={navItems} tabContents={tabContents} /> */}
         </div>
 
-        {/* Right Section - 25% width on laptop, hidden on smaller screens */}
-        <div className="w-full lg:w-[30%] h-screen overflow-y-auto">
-          <div className="flex flex-col items-center p-4 h-full">
-            {/* Time Card */}
-            <div className="w-full p-4 bg-[#292D32] rounded-2xl text-white flex justify-center items-center gap-3">
-              <div className="text-3xl font-bold lg:text-2xl">12:55 PM</div>
-              <div className="text-xs flex flex-col justify-center items-start">
-                Monday, <span>28 November</span>
+        <div class="flex-[3]">
+          <div className="w-full p-4 bg-[#292D32] rounded-[24px] text-white flex justify-center items-center gap-[1rem]">
+            <div className="time-text">12:55 PM</div>
+            <div className="day-date-text flex flex-col justify-center items-start">
+              Monday, <span>28 November</span>
+            </div>
+          </div>
+          <div className="dash-board-card storage">
+            <p className="dashboard-card-title">Storage</p>
+            <div className="storage-usage mt-2 flex justify-between items-start gap-2">
+              <div className="storage-usage-content w-[40%] mt-3">
+                <p className="flex items-center storagetext">
+                  <span className="w-2.5 h-2.5 rounded-full bg-[#0099FF] mr-2"></span>{' '}
+                  50% Used
+                </p>
+                <p className="flex items-center storagetext">
+                  <span className="w-2.5 h-2.5 rounded-full bg-[#E0EFDF] mr-2"></span>
+                  <br /> 50% Available
+                </p>
+              </div>
+              <div className="storage-usage-percentage w-[60%]">
+                <img src={img} alt="Storage Image" className="w-full h-auto" />
               </div>
             </div>
+          </div>
 
-            {/* Storage Card */}
-            <div className="p-3 bg-white rounded-2xl mt-6 w-full h-[300px] lg:mt-4 storage">
-              <h4 className="text-xl font-bold text-gray-600 lg:text-lg">Storage</h4>
-              <div className="flex justify-center items-center gap-[3rem]">
-                <div className="flex flex-col gap-5 mt-3 lg:mt-2">
-                  {/* Used */}
-                  <div className="flex items-center">
-                    <span className="w-3 h-3 rounded-full bg-blue-500 mr-2"></span>
-                    <span className="text-sm text-gray-500 font-semibold">
-                      50% Used
-                    </span>
-                  </div>
-                  {/* Available */}
-                  <div className="flex items-center">
-                    <span className="w-3 h-3 rounded-full bg-green-300 mr-2"></span>
-                    <span className="text-sm text-gray-500 font-semibold">
-                      50% Available
-                    </span>
-                  </div>
-                </div>
-                <div
-                  className="w-28 h-28 "
-                  style={{
-                    backdropFilter:
-                      'box-shadow(0px 0px 10px rgba(0, 0, 0, 0.1))',
-                  }}
-                >
-                  <Doughnut data={data} options={options} />
-                </div>
-              </div>
+          <div className="dash-board-card cpu-ram-usage">
+            <h3 className="card-title">CPU Ram usage</h3>
+            <div className="info">
+              <p>
+                <strong>CPU info:</strong> 11th gen Intel (R) Core (TM)
+                i5-1135G7 @2.4
+              </p>
+              <p>
+                <strong>Uptime:</strong> 11 minutes
+              </p>
+              <p>
+                <strong>CPU speed:</strong> 2,419.20 MHz
+              </p>
             </div>
-
-            {/* CPU and RAM Usage */}
-            <div className="p-3 bg-white rounded-2xl mt-6 w-full lg:mt-4 cpu_ram">
-              <h4 className="text-lg font-bold text-gray-500 mb-4">CPU Ram usage</h4>
-              <div className="w-full mt-3 lg:mt-2">
-                <div className="flex justify-start gap-2 mb-4">
-                  <span className="w-[100px] text-gray-500 text-xs">
-                    CPU info :
-                  </span>
-                  <span className=" text-gray-400 text-xs lg:text-[10px]">
-                    11th gen Intel (R) Core (TM) i5-1135G7 @ 2.40GHz
-                  </span>
+            <hr />
+            <div className="memory-usage">
+              <p className="section-title">Memory usage:</p>
+              <div className="usage-bar">
+                <span>CPU</span>
+                <div className="bar-container">
+                  <div className="bar cpu-bar" style={{ width: '7%' }}></div>
                 </div>
-                <div className="flex justify-start mb-4">
-                  <span className="w-[100px] text-gray-500 text-xs">
-                    Uptime :
-                  </span>
-                  <span className="text-gray-400 text-xs lg:text-[10px]" >11 minutes</span>
-                </div>
-                <div className="flex justify-start">
-                  <span className="w-[100px] text-gray-500 text-xs">
-                    CPU speed :
-                  </span>
-                  <span className="text-gray-400 text-xs lg:text-[10px]" >2,418.20 MHz</span>
-                </div>
+                <span>7%</span>
               </div>
-              <hr className="my-5"></hr>
-              <h3 className="text-lg font-bold text-gray-400 mb-5">Memory usage</h3>
-              <div className="w-full mt-3 lg:mt-2">
-                {/* CPU */}
-                <div className="flex justify-start gap-2 mb-5">
-                  <span className="w-[100px] text-gray-500 text-xs">CPU</span>
-                  <div className="flex flex-col gap-1 w-full">
-                    <div className="bg-gray-50 w-full h-3 rounded-[12px] overflow-hidden">
-                      <div className="bg-[#00ADEE] w-[20%] rounded-[8px] h-full"></div>
-                    </div>
-                    <span className="text-gray-400 text-xs">7%</span>
-                  </div>
+              <div className="usage-bar">
+                <span>RAM</span>
+                <div className="bar-container">
+                  <div className="bar ram-bar" style={{ width: '7%' }}></div>
                 </div>
-                {/* RAM */}
-                <div className="flex justify-start gap-2 mb-5">
-                  <span className="w-[100px] text-gray-500 text-xs">RAM</span>
-                  <div className="flex flex-col gap-1 w-full">
-                    <div className="bg-gray-50 w-full rounded-[12px] h-3 overflow-hidden">
-                      <div className="bg-green-500 w-[10%] rounded-[8px] h-full"></div>
-                    </div>
-                    <span className="text-gray-400 text-xs lg:text-[10px]">
-                      7% | 12,038.44 mb
-                    </span>
-                  </div>
+                <span>7% | 12,038.44 mb</span>
+              </div>
+              <div className="usage-bar">
+                <span>SWAP</span>
+                <div className="bar-container">
+                  <div className="bar swap-bar" style={{ width: '3%' }}></div>
                 </div>
-                {/* SWAP */}
-                <div className="flex justify-start gap-2 mb-5 w-full">
-                  <span className="w-[100px] text-gray-500 text-xs">SWAP</span>
-                  <div className="flex flex-col gap-1 w-full">
-                    <div className="bg-gray-50 w-full rounded-[12px] overflow-hidden h-3">
-                      <div className="bg-yellow-500 w-[10%] rounded-[8px] h-full"></div>
-                    </div>
-                    <span className="text-gray-400 text-xs">
-                      3% | 5,251.00 mb
-                    </span>
-                  </div>
-                </div>
+                <span>3% | 5,251.00 mb</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      <BottomNav />
     </>
   )
 }
