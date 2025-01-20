4f40fe8720d12476cdbca4acd2ba40685eb1a418
nikhilsinghpy <nikhilsingh1139@gmail.com>
2025-01-21 00:50:20 +0530

    sne

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -54,14 +54,101 @@ export const Dashboard = () => {
   return (
     <>
       <div className="flex w-full h-screen">
-      <div className="w-[77.49%] bg-gray-100 p-4">
-        <TabComponent navItems={navItems} tabContents={tabContents} />
+        <div className="w-[77.49%] bg-gray-100 p-4">
+          <TabComponent navItems={navItems} tabContents={tabContents} />
+        </div>
+        <div className="w-[22.51%]  p-4">
+          <div className="flex flex-col gap-[.8rem]">
+            {/* First Card */}
+            <div className="h-[7.9vh] bg-[#292D32] rounded-[12px] flex items-center justify-evenly text-white time-component">
+              <h3>12:00 AM</h3>
+              <span className="flex flex-col items-start">
+                <p>Monday</p>
+                <p>28 November</p>
+              </span>
+            </div>
+
+            {/* Second Card */}
+            <div className="h-[29.9vh] bg-[#FFFFFF] rounded-[12px] flex items-center justify-center p-4 ">
+              <div className="flex  gap-2 flex-col">
+                <h3 className='dashboard-card-title'>Storage</h3>
+                <div className='flex'>
+                <div className="storage-usage-content w-[40%] mt-3">
+                <p className="flex items-center storagetext">
+                  <span className="w-2.5 h-2.5 rounded-full bg-[#0099FF] mr-2"></span>{' '}
+                  50% Used
+                </p>
+                <p className="flex items-center storagetext">
+                  <span className="w-2 h-2 rounded-full bg-[#E0EFDF] mr-2"></span>
+                  <br /> 50% Available
+                </p>
+              </div>
+                <div className="storage-usage-percentage w-[60%]">
+                  <img
+                    src={img}
+                    alt="Storage Image"
+                    className="w-full h-auto"
+                  />
+                </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Third Card */}
+            <div className="h-[53vh] bg-[#FFFFFF] rounded-[12px] flex p-4 flex-col gap-3">
+              <h3 className='dashboard-card-title'>CPU Ram usage</h3>
+              <span className='flex justify-start gap-2 items-center'>
+                <h6 className='device-info-title'>CPU info : </h6>
+                <p className='device-info-content'>11th gen Intel (R) Core (TM) i5-1135G7 @2.4</p>
+              </span>
+              <span className='flex justify-start gap-2 items-center'>
+                <h6 className='device-info-title'>Uptime : </h6>
+                <p className='device-info-content'>11 minutes</p>
+              </span>
+              <span className='flex justify-start gap-2 items-center'>
+                <h6 className='device-info-title'>CPU speed : </h6>
+                <p className='device-info-content'>2,419.20 MHz</p>
+              </span>
+
+              <hr className='text-[#EDEDFF] bg-[#EDEDFF] h-[2px]'/>
+              <h6 className='device-info-title'>Memory usage : </h6>
+              <div className="usage-card flex items-start justify-start gap-2">
+                <span className=' usage-card'>
+                  CPU
+                </span>
+                <div className='flex flex-col w-full'>
+                <div className="progres-bar w-full bg-[#F2F2F2]  rounded-[10px] overflow-hidden">
+                  <div className="progress-bar-inner bg-[#00ADEE] w-[15%] h-full" style={{borderTopRightRadius:"10px" , borderBottomRightRadius:"10px"}}></div>
+                </div>
+                <span className='text-black status-text'>7%</span>
+                </div>
+              </div>
+              <div className="usage-card flex items-start justify-start gap-2">
+                <span className='usage-card'>
+                RAM
+                </span>
+                <div className='flex flex-col w-full'>
+                <div className="progres-bar w-full bg-[#F2F2F2]  rounded-[10px] overflow-hidden">
+                  <div className="progress-bar-inner bg-[#64AE5F] w-[15%] h-full" style={{borderTopRightRadius:"10px" , borderBottomRightRadius:"10px"}}></div>
+                </div>
+                <span className='text-black status-text'>7% | 12,038.44 mb</span>
+                </div>
+              </div>
+              <div className="usage-card flex items-start justify-start gap-2">
+                <span className='usage-card'>
+                SWAP
+                </span>
+                <div className='flex flex-col w-full '>
+                <div className="progres-bar w-full bg-[#F2F2F2]  rounded-[10px] overflow-hidden">
+                  <div className="progress-bar-inner bg-[#FEC610] w-[15%] h-full" style={{borderTopRightRadius:"10px" , borderBottomRightRadius:"10px"}}></div>
+                </div>
+                <span className='text-black status-text'>3% | 5,251.00 mb</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="w-[22.51%] bg-gray-200 p-4">
-        <h2 className="text-xl font-semibold">Right Container</h2>
-        <p>This is the side section content.</p>
-      </div>
-    </div>
     </>
   )
 }
